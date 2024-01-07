4b97be0f53e1cbd49c5f7233a35e6cbe3f93aed2
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-01-07 12:27:51 +0100

    [feat] add paraglide to the repo

--- src/pages/content/[id].js
+++ src/pages/content/[id].js
@@ -1,10 +1,17 @@
 import { useRouter } from "next/router"
+import * as m from "../../paraglide/messages"
 
 const ContentPage = () => {
     const router = useRouter()
     const contentId = router.query.id
-    
-    return <p>Content: {contentId}</p>
+
+    const markdown = m.hello_world()
+    console.log(markdown)
+
+    return <div>
+        <p>Content: {contentId}</p>
+        <p>Markdown: {markdown}</p>
+    </div>
 }
 
 export default ContentPage
