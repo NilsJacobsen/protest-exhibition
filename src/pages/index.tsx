2b46e170a02778dfe3607106877af298e0fa7470
Nils Jacobsen <nils.jacobsen98@gmail.com>
2024-01-28 20:59:28 +0100

    Add title and meta description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
 import Image from 'next/image'
+import Head from 'next/head'
 
 export default function Home() {
   return (
+    <>
+    <Head >
+        <title>
+          Auf die Straße | Ausstellung Dortmund
+        </title>
+        <meta
+          name="description"
+          content="Setze ein Statement für Veränderung. Wir gestalten aktiv eine nachhaltige Zukunft. Sei dabei!"
+          key="desc"
+        />
+      </Head>
     <main
       className={`w-full h-screen flex flex-col items-center justify-center p-10 font-venusmartre`}
     >
@@ -10,5 +22,7 @@ export default function Home() {
       <p className='text-4xl'>Auf die Straße</p>
       <p className='font-rubik pt-3'>07.03 - 24.03</p>
     </main>
+    </>
+    
   )
 }
